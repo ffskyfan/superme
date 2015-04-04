3bc5c3a3bf6bfbb9ba337911f32d3a27c273b573
ffskyfan <ffskyfan@gmail.com>
2015-04-05 02:18:51 +0800

    add 'word' table for database use migration

---
+++ database/migrations/2015_04_04_075829_create_words_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('words', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('type');
+            $table->string('kanji', 60);
+            $table->text('translation');
+            $table->rememberToken();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('words');
+	}
+
+}
