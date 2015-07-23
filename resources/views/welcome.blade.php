194a3e934a3faf9f7f572c5185066cd5a83b16c9
ffskyfan <ffskyfan@gmail.com>
2015-07-23 23:08:33 +0800

    change title of web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Deni Love Achi</title>
+		<title>Deni♥Achi</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Deni Love Achi</div>
+				<div class="title">Deni♥Achi</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
